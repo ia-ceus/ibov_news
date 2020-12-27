2f908e80d6864509a41e08ff18c56e372d28d37d
Mateus Leite Pucciarelli <mateuslpucciarelli@gmail.com>
2020-12-27 12:54:26 -0200

    início script

--- ibov_news/ibov_news.py
+++ ibov_news/ibov_news.py
@@ -6,3 +6,56 @@ Grupo de Estudos de Inteligência Artificial - CEUS
 
 """
 
+import pandas as pd
+import numpy as np
+
+import yfinance as yf
+
+DELTA = 0.01
+DATA_INI = '2000-01-01'
+DATA_FIM = '2020-11-01'
+TICKER = '^BVSP'
+
+# Funções para criar target
+
+def target_close_maior_open(row, delta):
+  if row['Close'] > (1+delta) * row['Open']:
+    target = 1
+  else:
+    target = 0
+  return target
+
+def target_high_maior_open(row, delta):
+  if row['High'] > (1+delta) * row['Open']:
+    target = 1
+  else:
+    target = 0
+  return target
+
+def target_high_igual_open(row, delta):
+  if row['High'] == row['Open']:
+    target = 1
+  else:
+    target = 0
+  return target
+
+def target_high_menor_open(row, delta):
+  if row['High'] <= (1+delta) * row['Open']:
+    target = 1
+  else:
+    target = 0
+  return target
+
+# aplicar target
+
+def criar_target(df, tipo=target_close_maior_open, delta=0.01):
+
+    return df.apply(lambda linha: tipo(linha, delta), axis=1)
+
+
+# baixar dataset yfinance
+
+def download_yf(tkr='^BVSP', dti='2000-01-01', dtf='2020-11-01'):
+    
+    return yf.download(ticker, start=dti, end=dtf)
+
